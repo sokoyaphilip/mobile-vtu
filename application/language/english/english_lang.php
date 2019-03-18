78bc46849c4e40841fbff387158dc4d4f9f7e2e9
Sokoya Philip <philo4u2c@gmail.com>
2019-03-18 09:11:05 +0100

    Okebanu

--- application/language/english/english_lang.php
+++ application/language/english/english_lang.php
@@ -4,7 +4,7 @@ $lang['site_state'] = 'development';
 $lang['app_name'] = 'Gecharl.com';
 $lang['app_email'] = 'hello@gecharl.com';
 $lang['contact_no'] = '08066795128';
-$lang['address'] = 'kebanu Plaza besides 2nd Salvation Ministries, <br />Elikpokwu-Odu Aluu link Road, Rukpokwu, <br />PortHarcourt Rivers State.';
+$lang['address'] = 'Okebanu Plaza besides 2nd Salvation Ministries, <br />Elikpokwu-Odu Aluu link Road, Rukpokwu, <br />PortHarcourt Rivers State.';
 $lang['banks'] = 'Access Bank Plc, Citibank Nigeria Limited, Diamond Bank Plc, Ecobank Nigeria Plc, Enterprise Bank , Fidelity Bank Plc, FIRST BANK NIGERIA LIMITED, First City Monument Bank Plc, Guaranty Trust Bank Plc, Heritage Banking Company Ltd, Key Stone Bank, MainStreet Bank, Skye Bank Plc, Stanbic IBTC Bank Ltd, Standard Chartered Bank Nigeria Ltd, Sterling Bank Plc, SunTrust Bank Nigeria Limited, Union Bank of Nigeria Plc, United Bank For Africa Plc, Unity Bank Plc, Wema Bank Plc, Zenith Bank Plc';
 
 //Mtn 85%
