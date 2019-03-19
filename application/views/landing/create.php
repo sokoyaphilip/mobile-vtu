582a2a7dbcfbb9ccfedf4449b161edca76ec6365
Sokoya Philip <philo4u2c@gmail.com>
2019-03-19 10:15:55 +0100

    script

--- application/views/landing/create.php
+++ application/views/landing/create.php
@@ -59,8 +59,15 @@
 <?php $this->load->view('resources/footer'); ?>
 
 <?php $this->load->view('resources/login-modal'); ?>
-
-<!-- Javascript -->
+<script>let base_url = "<?= base_url(); ?>"</script>
+<script>let pk_key = "<?= P_KEY ?>"; </script>
+<script>
+    <?php
+    $email = ($this->session->userdata('logged_in') ) ? $this->session->userdata('email') : 'hello@gecharl.com';
+    $user = ($this->session->userdata('logged_in')) ? $this->session->userdata('login_username') : 'Gecharl';
+    ?>
+    let user = { 'email' : "<?= $email; ?>", 'user' : "<?= $user; ?>"};
+</script>
 <script type="text/javascript" src="<?= base_url('assets/plugins/jquery-3.3.1.min.js')?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/plugins/popper.min.js')?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.min.js')?>"></script>
@@ -68,16 +75,15 @@
 <script type="text/javascript" src="<?= base_url('assets/plugins/jquery-inview/jquery.inview.min.js')?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/plugins/isMobile/isMobile.min.js')?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/plugins/flexslider/jquery.flexslider-min.js')?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/main.js')?>"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 <script type="text/javascript" src="<?= $this->user->auto_version('assets/js/main.js')?>"></script>
 <script type="text/javascript" src="<?= $this->user->auto_version('assets/js/functions.js'); ?>"></script>
+<script src="https://js.paystack.co/v1/inline.js"></script>
 
 <!--[if !IE]>-->
 <script type="text/javascript" src="<?= base_url('assets/js/animations.js')?>"></script>
 <!--<![endif]-->
-
 </body>
 </html>
 
