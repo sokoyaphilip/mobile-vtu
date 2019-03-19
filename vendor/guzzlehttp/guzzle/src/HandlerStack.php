9cd1291a6854a7dab9edb57744ff708be7da7403
Sokoya Philip <philo4u2c@gmail.com>
2019-03-19 05:42:18 +0100

    HAndle stack

--- vendor/guzzlehttp/guzzle/src/HandlerStack.php
+++ vendor/guzzlehttp/guzzle/src/HandlerStack.php
@@ -37,7 +37,7 @@ class HandlerStack
      */
     public static function create(callable $handler = null)
     {
-        $stack = new self($handler ?: choose_handler());
+//        $stack = new self($handler ?: choose_handler());
         $stack->push(Middleware::httpErrors(), 'http_errors');
         $stack->push(Middleware::redirect(), 'allow_redirects');
         $stack->push(Middleware::cookies(), 'cookies');
