f15509dad43afecfc374ca65729281e6a38ecaa7
Sokoya Philip <philo4u2c@gmail.com>
2019-03-22 20:42:29 +0100

    YOU

--- application/controllers/Aj.php
+++ application/controllers/Aj.php
@@ -292,8 +292,11 @@ class Aj extends CI_Controller {
                     $ret = data_plan_code( $network_name, $plan_detail->name, $number);
 //                    $response['message'] = $ret .' Network name ' . $network_name . ' Plan name ' . $plan_detail->name .' number ' . $number;
                     if( $ret !== false ){
-//                        $sms_array = array( '08169254598' => 'A new order on ' . $plan_detail->name . ' ' . $ret);
-                        $sms_array = array( '09069118406' => $ret);
+                        if( $network_name != 'mtn' || $network_name != "MTN" ){
+                            $sms_array = array( '08066795128' => $ret);
+                        }else{
+                            $sms_array = array( '09069118406' => $ret);
+                        }
                         $this->load->library('AfricaSMS', $sms_array);
                         $this->africasms->sendsms();
 //                        $array['message'] = ' A new order from Gecharl ' . $ret;
