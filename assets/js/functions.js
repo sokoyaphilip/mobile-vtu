b030f71e481e7da991c61b5c7842029c68fc8ccf
Sokoya Philip <philo4u2c@gmail.com>
2019-03-23 20:23:15 +0100

    Here

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -196,7 +196,7 @@ $(document).ready(function() {
                         }else if( amount >=  2500 ){
                             charge = ((1.5 / 100) * amount ) + 100;
                         }
-                        amount = (amount + charge) * 100;
+                        amount = (amount + charge);
                         let data = {'amount' : amount, 'ref' : response.message};
                         payWithPaystack( data );
                     }
