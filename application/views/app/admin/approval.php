55bc1a8385cfa1f408b67672ce34913f46321969
Sokoya Philip <philo4u2c@gmail.com>
2019-03-23 23:12:51 +0100

    GIT

--- application/views/app/admin/approval.php
+++ application/views/app/admin/approval.php
@@ -56,7 +56,7 @@
                                                             <?= $funding->trans_id; ?>
                                                             <?php
                                                                 $check = $this->site->run_sql("SELECT id FROM transaction_status WHERE tid = {$funding->trans_id}")->num_rows();
-                                                            if( count($check)) : ?>
+                                                            if( $check > 0 ) : ?>
                                                                 <span><a href="<?= base_url('admin/confirm_payment/?tid='. $funding->trans_id);?>">Confirm Payment</a></span>
                                                             <?php endif;?>
                                                         </td>
