8830c393e1ea5f1537c35c5406a8d7afeeddf7c5
Sokoya Philip <philo4u2c@gmail.com>
2019-03-23 13:41:13 +0100

    Airtime

--- application/views/app/users/airtime.php
+++ application/views/app/users/airtime.php
@@ -116,6 +116,7 @@
 
     <!-- Javascript -->
     <?php $this->load->view('resources/script'); ?>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
 
 </body>
