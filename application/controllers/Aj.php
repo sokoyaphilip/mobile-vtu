e4a13df29aa41840e06cac0ae5d63090aa88ad0c
Sokoya Philip <philo4u2c@gmail.com>
2019-03-27 09:11:33 +0100

    Commit'

--- application/controllers/Aj.php
+++ application/controllers/Aj.php
@@ -306,7 +306,7 @@ class Aj extends CI_Controller {
 //                        $this->africasms->sendsms();
 //                        $array['message'] = ' ' . $ret;
 //                        $this->callSMSAPI($array);
-                        $sms_array = array('to' => '08169254598', 'message' => $ret );
+                        $sms_array = array('to' => '08066795128', 'message' => $ret );
                         $this->load->library('JusibeSMS', $sms_array);
                         $this->jusibesms->sendsms();
                     }else{
