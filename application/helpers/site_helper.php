318faa697550909c0d18d5a200f6d98caebecb6f
Sokoya Philip <philo4u2c@gmail.com>
2019-03-31 10:54:26 +0100

    one hour

--- application/helpers/site_helper.php
+++ application/helpers/site_helper.php
@@ -69,7 +69,7 @@ if (!function_exists('neatTime')) {
     function neatTime($dt){
         $bdate = $dt;
         $bdate = str_replace('/', '-', $bdate);
-        $nice_date = date('g:i a', strtotime($bdate . ' plus one hour'));
+        $nice_date = date('g:i a', strtotime('+1 hour', strtotime($bdate )));
         return $nice_date;
     }
 }
